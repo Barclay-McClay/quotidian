8355758ed720c685e7dabdf0f04a986f58c3b886
Barclay-McClay <102266976+Barclay-McClay@users.noreply.github.com>
2024-06-06 00:32:32 +1000

    added blog index page

--- script.js
+++ script.js
@@ -2,37 +2,45 @@ fetch('./blog-posts/blog-data.json')
   .then(response => response.json())
   .then(data => {
     const container = document.querySelector('.grid-container');
-    
+
     data.posts.sort((a, b) => new Date(b.date) - new Date(a.date)); // Sort by date
-    
+    // 5 most Recent posts
+    data.posts = data.posts.slice(0, 5);
+
     data.posts.forEach(post => {
-      let item = document.createElement('div');
-      item.className = 'grid-item';
-
-      let link = document.createElement('a'); // New anchor tag
-      link.href = post.url; // Link to the post
-
-      let image = document.createElement('img');
-      image.src = `./blog-posts/blog-images/${post.image}`;
-      image.className = 'blog-img';
-
-      let overlay = document.createElement('div');
-      overlay.className = 'overlay';
-
-      let title = document.createElement('h2');
-      title.innerText = post.title;
-      title.className = 'post-title';
-      
-      let date = document.createElement('p');
-      date.innerText = post.date;
-      date.className = 'post-date';
-
-      overlay.append(title, date);
-      
-      link.append(image, overlay); // Append image and overlay to link instead of item
-
-      item.append(link); // Append the link to item
-
-      container.appendChild(item);
+      createPost(post, container);
     });
+
   });
+
+
+function createPost(post, container) {
+  let item = document.createElement('div');
+  item.className = 'grid-item';
+
+  let link = document.createElement('a'); // New anchor tag
+  link.href = post.url; // Link to the post
+
+  let image = document.createElement('img');
+  image.src = `./blog-posts/blog-images/${post.image}`;
+  image.className = 'blog-img';
+
+  let overlay = document.createElement('div');
+  overlay.className = 'overlay';
+
+  let title = document.createElement('h2');
+  title.innerText = post.title;
+  title.className = 'post-title';
+  
+  let date = document.createElement('p');
+  date.innerText = post.date;
+  date.className = 'post-date';
+
+  overlay.append(title, date);
+  
+  link.append(image, overlay); // Append image and overlay to link instead of item
+
+  item.append(link); // Append the link to item
+
+  container.appendChild(item);
+}
